435564c8d060bdbfa1e35fb162c77f79bee5a74f
“BethFieldgit config --global user.email “bf123@hotmail.co.uk <bf123@hotmail.co.uk>
2022-11-10 15:30:04 +0000

    added cards

--- src/components/TimelineCard/index.jsx
+++ src/components/TimelineCard/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import "./index.css";
 
 const TimelineCard = ({ id, order, imgPath, date, description }) => {
   return (
-    <div className="card">
-      <img src={imgPath} />
-      <p>{description}</p>
-    </div>
+    <Card className="card" id={id} order={order} date={date} description={description}>
+      <CardMedia
+        component="img"
+        height="194"
+        image="../../assets/placeholder.png"
+        alt="placeholder"
+      />
+      <CardContent>
+        <Typography>{description}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 
 export default TimelineCard;
+
+// .card {
+//   width: 150px;
+//   height: 200px;
+//   background-color: aliceblue;
+// }
--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Timeline, TimelineCard } from "../../components";
+
 import "./index.css";
 
 const TimelinePage = () => {
   const [cards, setCards] = useState([]);
   const [dots, setDots] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
+  const { id } = useParams();
+  console.log(id);
+
   useEffect(() => {
-    async function loadCards() {
-      const response = await fetch("http://localhost:3000/games/timeline");
-      const cardData = response.json();
+    async function loadGame() {
+      const response = await fetch(`http://localhost:5432/games/timeline/${id}`);
+      const gameData = await response.json();
+      console.log("game data", gameData);
+      const cardData = gameData.cards;
       setCards(cardData);
+      console.log("cards", cards);
     }
-    loadCards();
+    loadGame();
   }, []);
 
   function displayCards() {
     return cards.map((card) => (
       <TimelineCard
         key={card}
-        id={card.id}
-        order={card.order}
-        imgPath={card.imgPath}
-        date={card.date}
-        description={card.description}
+        id={card.card_id}
+        order={card.card_order}
+        imgPath={card.card_img_path}
+        date={card.card_date}
+        description={card.card_description}
       />
     ));
   }
@@ -31,7 +39,7 @@ const TimelinePage = () => {
   return (
     <main className="timeline-main">
       <Timeline dots={dots} setDots={setDots} />
-      <div className="card-container">{displayCards()}</div>
+      {displayCards()}
     </main>
   );
 };
