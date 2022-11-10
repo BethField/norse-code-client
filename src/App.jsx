8d248e5ff426ceded594a1fd09a450cad4cfc9e5
“BethFieldgit config --global user.email “bf123@hotmail.co.uk <bf123@hotmail.co.uk>
2022-11-10 09:27:57 +0000

    started timeline game page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { AppBar } from './components'
-import { Routes, Route } from 'react-router-dom';
-import { PageWrapper } from './components'
+import { AppBar } from "./components";
+import { Routes, Route } from "react-router-dom";
+import { PageWrapper } from "./components";
+import { TimelinePage } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<PageWrapper />}>
+          <Route path="/games/timeline" element={<TimelinePage />} />
         </Route>
       </Routes>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,4 @@
-export { default as AppBar } from './AppBar'
-export { default as PageWrapper } from './PageWrapper'
+export { default as AppBar } from "./AppBar";
+export { default as PageWrapper } from "./PageWrapper";
+export { default as Timeline } from "./Timeline";
+export { default as TimelineCard } from "./TimelineCard";
