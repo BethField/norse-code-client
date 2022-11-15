9ea5db222fcdd01899d8702cdeebb4883acba45b
Kai Stevens <kai.stevens97@gmail.com>
2022-11-15 10:32:04 +0000

    remove bloat, tidy up the code, add correct page in index.jsx

--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { DndContext, DragOverlay } from '@dnd-kit/core';
+import { DndContext, DragOverlay } from "@dnd-kit/core";
 
 import { Timeline, TimelineCard, TimelineHeader } from "../../components";
 import { DraggableItem } from "../../components/DraggableItem";
@@ -17,22 +17,21 @@ const TimelinePage = () => {
   useEffect(() => {
     async function loadGame() {
       try {
-          const response = await fetch(`http://localhost:3000/games/timeline/${id}`);
-          const gameData = await response.json();
-          const cardData = gameData.cards;
-    
-          const shuffledCards = cardData
-            .map((value) => ({ value, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value);
+        const response = await fetch(
+          `http://localhost:3000/games/timeline/${id}`
+        );
+        const gameData = await response.json();
+        const cardData = gameData.cards;
 
-    
-          setCards(shuffledCards);
+        const shuffledCards = cardData
+          .map((value) => ({ value, sort: Math.random() }))
+          .sort((a, b) => a.sort - b.sort)
+          .map(({ value }) => value);
 
+        setCards(shuffledCards);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-
     }
     loadGame();
   }, []);
@@ -40,128 +39,127 @@ const TimelinePage = () => {
   // Setup Draggable and Droppable states -> Currently hardcoded
   const createInitialStates = (type) => {
     const len = 9;
-    const initialStates = {}
-    for (let i = 1; i < len+1; i++) {
-      type == "draggable" ? initialStates[i] = null : initialStates[i] = {filled: false, correct: false};
+    const initialStates = {};
+    for (let i = 1; i < len + 1; i++) {
+      type == "draggable"
+        ? (initialStates[i] = null)
+        : (initialStates[i] = { filled: false, correct: false });
     }
-    return initialStates
-  }
-
-  // CURRENT AIM : SET UP STATE AND DROPPING IN FUNCTIONALITY
+    return initialStates;
+  };
 
   const droppableInitialStates = createInitialStates("droppable");
   const draggableInitialStates = createInitialStates("draggable");
 
-  const [objectContainerStates, setObjectContainerStates] = useState(droppableInitialStates);
+  const [objectContainerStates, setObjectContainerStates] = useState(
+    droppableInitialStates
+  );
   const [dnDPairings, setDndPairings] = useState(draggableInitialStates);
 
   // below function, has same role as checkInitialState (basically, checks the parents, if they're null render inside the container div)
   function displayCards() {
     const entries = Object.entries(dnDPairings);
 
-    const newEntries = entries.map(entry => {
-      if(entry[1] == null) {
-        return <DraggableItem id={entry[0]}><TimelineCard
-                                              key={cards[entry[0] - 1].card_id}
-                                              id={cards[entry[0] - 1].card_id}
-                                              order={cards[entry[0] - 1].card_order}
-                                              imgPath={cards[entry[0] - 1].card_img_path}
-                                              date={cards[entry[0] - 1].card_date}
-                                              description={cards[entry[0] - 1].card_description}
-        /></DraggableItem>
+    const newEntries = entries.map((entry) => {
+      if (entry[1] == null) {
+        return (
+          <DraggableItem id={entry[0]}>
+            <TimelineCard
+              key={cards[entry[0] - 1].card_id}
+              id={cards[entry[0] - 1].card_id}
+              order={cards[entry[0] - 1].card_order}
+              imgPath={cards[entry[0] - 1].card_img_path}
+              date={cards[entry[0] - 1].card_date}
+              description={cards[entry[0] - 1].card_description}
+            />
+          </DraggableItem>
+        );
       }
     });
     return newEntries;
   }
 
-  // const checkDragInDrop = (droppableAreaId) => {
-  //   // get keys
-  //   const entries = Object.entries(dnDPairings);
-  //   const newEntries = entries.map(entry => {
-  //     if (droppableAreaId == entry[1]) {
-  //       return <DraggableItem id={entry[0]} key={entry[0]}>{entry[0]}</DraggableItem>
-  //     } else {
-  //       return null;
-  //     }
-  //   });
-
-  //   return newEntries;
-  // }
-
   function handleDragStart(event) {
     console.log(event);
-    setActiveId((event.active.id - 1));
+    setActiveId(event.active.id - 1);
   }
 
   function handleDragEnd(event) {
-    const {over, active} = event;
+    const { over, active } = event;
 
     // copied this over
     setActiveId(null);
     // firstly, check if an active item has a parent, if it does set that parent to false
-    if(dnDPairings[active.id] || dnDPairings[active.id] == 0) {
-      setObjectContainerStates(prevState => ({
+    if (dnDPairings[active.id] || dnDPairings[active.id] == 0) {
+      setObjectContainerStates((prevState) => ({
         ...prevState,
-        
+
         [dnDPairings[active.id]]: {
           ...prevState[dnDPairings[active.id]],
-          ["filled"]: false
-        }
+          ["filled"]: false,
+        },
       }));
     }
-    
+
     if (over && !objectContainerStates[over.id]["filled"]) {
       // change the container filled status to true
-      setObjectContainerStates(prevState => ({
+      setObjectContainerStates((prevState) => ({
         ...prevState,
         [over.id]: {
           ...prevState[over.id],
-          ["filled"]: true
-        }
-
+          ["filled"]: true,
+        },
       }));
 
       // update the parent of the draggable item to be the id of the droppable area
-      setDndPairings(prevState => ({
+      setDndPairings((prevState) => ({
         ...prevState,
-        [active.id]: over.id
+        [active.id]: over.id,
       }));
     } else {
-
       // turn the parent to null (render the draggable item outside)
-      setDndPairings (prevState => ({
+      setDndPairings((prevState) => ({
         ...prevState,
-        [active.id]: null
+        [active.id]: null,
       }));
     }
   }
 
-  console.log("before return even runs, dndpairings", dnDPairings);
   return (
     // create, handleDragEnd function for DnDContext
     <DndContext onDragEnd={handleDragEnd} onDragStart={handleDragStart}>
       <main className="timeline-page">
         <TimelineHeader />
         <div className="timeline-container">
-          <Timeline dots={dots} setDots={setDots} cards={cards} objectContainerStates={objectContainerStates} dnDPairings={dnDPairings}/>
+          <Timeline
+            dots={dots}
+            setDots={setDots}
+            cards={cards}
+            objectContainerStates={objectContainerStates}
+            dnDPairings={dnDPairings}
+          />
         </div>
         <div className="card-container">
           {/* This is our scrollable list (where we render the ACTUAL draggable items) */}
-          {cards.length != 0 ? <div className="card-scroll-box">{displayCards()}</div> : <div>Loading...</div>}
+          {cards.length != 0 ? (
+            <div className="card-scroll-box">{displayCards()}</div>
+          ) : (
+            <div>Loading...</div>
+          )}
         </div>
-        
+
         <DragOverlay>
-          
-          {(activeId || activeId === 0) ? ( <TimelineCard
-            key={cards[activeId].card_id}
-            id={cards[activeId].card_id}
-            order={cards[activeId].card_order}
-            imgPath={cards[activeId].card_img_path}
-            date={cards[activeId].card_date}
-            description={cards[activeId].card_description} />) : null}
-
+          {activeId || activeId === 0 ? (
+            <TimelineCard
+              key={cards[activeId].card_id}
+              id={cards[activeId].card_id}
+              order={cards[activeId].card_order}
+              imgPath={cards[activeId].card_img_path}
+              date={cards[activeId].card_date}
+              description={cards[activeId].card_description}
+            />
+          ) : null}
         </DragOverlay>
-
       </main>
     </DndContext>
   );
